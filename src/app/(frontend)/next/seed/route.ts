defdad8bab7c496903374052cccc0d90bdcdcda4
ajoysr <ajoy.sarker@brainstation-23.com>
2026-01-01 19:00:21 +0600

    refactor: Update POST handler to use NextRequest type.

--- src/app/(frontend)/next/seed/route.ts
+++ src/app/(frontend)/next/seed/route.ts
@@ -1,3 +1,4 @@
+import { NextRequest } from 'next/server'
 import { createLocalReq, getPayload } from 'payload'
 import { seed } from '@/endpoints/seed'
 import config from '@payload-config'
@@ -7,13 +8,7 @@ const payloadToken = 'payload-token'
 export const maxDuration = 60 // This function can run for a maximum of 60 seconds
 
 export async function POST(
-  req: Request & {
-    cookies: {
-      get: (name: string) => {
-        value: string
-      }
-    }
-  },
+  req: NextRequest,
 ): Promise<Response> {
   const payload = await getPayload({ config })
   const requestHeaders = await headers()
